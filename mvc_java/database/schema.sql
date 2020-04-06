bf35eadb71b2bcaad89b728151217f6caa503d55
Ryan Quigley <ryan.quigley32@outlook.com>
2020-04-06 11:59:10 -0400

    database start

--- mvc_java/database/schema.sql
+++ mvc_java/database/schema.sql
@@ -5,14 +5,38 @@
 BEGIN;
 
 -- CREATE statements go here
+DROP TABLE IF EXISTS family;
 DROP TABLE IF EXISTS app_user;
+DROP TABLE IF EXISTS friends;
+
+CREATE TABLE family (
+  family_id SERIAL NOT NULL,
+  family_name varchar(100) NOT NULL,
+
+  CONSTRAINT pk_family_id PRIMARY KEY(family_id)
+);
+
 
 CREATE TABLE app_user (
   id SERIAL PRIMARY KEY,
   user_name varchar(32) NOT NULL UNIQUE,
   password varchar(32) NOT NULL,
   role varchar(32),
-  salt varchar(255) NOT NULL
+  salt varchar(255) NOT NULL,
+  first_name varchar(100) NOT NULL,
+  last_name varchar(100) NOT NULL,
+  friend_id int,
+  family_id int NOT NULL,
+
+  CONSTRAINT fk_family_id FOREIGN KEY (family_id) REFERENCES family (family_id)
 );
+
+
+CREATE TABLE friends (
+  user_id integer NOT NULL,
+  friend_id integer NOT NULL
+);
+
+
 
 COMMIT;
