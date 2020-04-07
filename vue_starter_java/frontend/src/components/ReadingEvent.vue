dbff526dc0ce70ceff9031dee45fc40d7235df9c
Leigh Hartmann <lahartmann@gmail.com>
2020-04-07 19:27:41 -0400

    ReadingEvent component started with book search api

--- vue_starter_java/frontend/src/components/ReadingEvent.vue
+++ vue_starter_java/frontend/src/components/ReadingEvent.vue
@@ -1,14 +1,70 @@
 <template>
-
+  <div>
+    <div class="header">
+      <h2>Book Search</h2>
+    </div>
+    <div class="form">
+      <div class="form-input">
+        <span class="label">Enter Book ISBN:</span> <input type="text" v-model="book.isbn" placeholder="Enter ISBN Here">
+      </div>
+      <button :disabled="!isValidForm" v-on:click="searchBooks">Search</button>
+    </div>
+  </div>
 </template>
-
-
 
 <script>
 
+import axios from 'axios'
+
+export default {
+  props: {
+    apiURL: String
+
+  },
+  data() {
+    return {
+      book: {
+        isbn: '',
+        title: '',
+        author: '',
+        thumbnail: '',
+      }
+    }
+  },
+  methods: {
+    
+    addBook() {
+      axios.get('https://openlibrary.org/api/books?format=json&jscmd=data&bibkeys=ISBN:' + book.isbn)
+
+      .then(response => {
+        (this.book = response.data)
+      
+      })
+      .catch(error => {
+        console.log(error + ' there was an error')
+      })
+    },
+    
+  //   updateReview() {
+  //     axios.put(`${this.apiURL}/${this.reviewID}`,this.review)
+  //     .then((response) => {
+  //       if( response.status === 204 ) {
+  //         this.$emit('showReviews');
+  //       }
+  //     })
+  //     .catch(error => {
+  //       console.log(error + ' there was an error')
+  //     })
+  //   }
+  // },
+  computed: {
+    isValidForm() {
+      return this.book.isbn != '';
+    },
+    
+  
+};
 </script>
-
-
 
 <style>
 
