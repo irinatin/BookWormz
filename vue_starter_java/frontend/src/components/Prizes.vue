b7cef2195fdb55cff2328ffe217c2757696d354f
Adam Myers <asmyers1121@gmail.com>
2020-04-08 16:57:14 -0400

    Skeleton components

--- vue_starter_java/frontend/src/components/Prizes.vue
+++ vue_starter_java/frontend/src/components/Prizes.vue
@@ -1,11 +1,62 @@
 <template>
+<div>
+    <div class="title">
+        <h2>PRIZES!</h2>
+    </div>
 
+        <div class="active_prizes">
+        <!-- Display all current prizes -->
+        </div>
+
+            <div class="create_new_prize">
+                <form class="new_prize_form">
+                    <label for="prize_name">Prize Name</label><br>
+                    <input type="text" id="prize_name" name="prize_name" placeholder="Enter Prize Name"><br>
+                    <label for="description">Description</label><br>
+                    <input type="text" id="description" name="description" placeholder="Enter Prize Description"><br>
+                    <label for="milestone">Milestone (minutes reading)</label><br>
+                    <input type="text" id="milestone" name="milestone" placeholder="Enter Milestone"><br>
+                    <label for="user_group">User Group (Parent/Child)</label><br>
+                    <input type="text" id="user_group" name="user_group" placeholder="Enter User Group"><br>
+                    <label for="max_prizes">Prize Cap</label><br>
+                    <input type="text" id="max_prizes" name="max_prizes" placeholder="Enter Prize Cap"><br>
+                    <label for="start_date">Start Date</label><br>
+                    <input type="text" id="start_date" name="start_date" placeholder="Enter Prize Start Date"><br>
+                    <label for="end_date">End Date</label><br>
+                    <input type="text" id="end_date" name="end_date" placeholder="Enter Prize End Date"><br>
+            </div>
+
+                <div class="edit_active_prize">
+
+                </div>
+</div>
 </template>
 
 
 
 <script>
 
+import axios from 'axios'
+
+export default{
+    props:{
+
+    },
+    
+    data() {
+        return{
+
+        }
+    }
+},
+
+methods:{
+
+},
+
+computed: {
+
+},
 </script>
 
 
--- vue_starter_java/frontend/src/views/Home.vue
+++ vue_starter_java/frontend/src/views/Home.vue
@@ -3,7 +3,12 @@
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
     
+
+            
+
   </div>
+
+
 </template>
 
 <script>
@@ -11,3 +16,15 @@ export default {
   name: 'home'
 }
 </script>
+
+<style scoped>
+body {
+  margin: 0px;
+  padding: 400px 100px;
+  background: linear-gradient(to bottom, rgba(102, 255, 102, 0.7), rgba(0, 128, 0, 0.25));
+  background-size: cover;
+  background-position: center;
+  font-family: sans-serif;
+  height: 100%;
+}
+</style>
