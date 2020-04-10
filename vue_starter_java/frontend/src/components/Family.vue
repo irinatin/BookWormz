44ea16897102640cdaaf256fbc9f3a0974740326
Ryan Quigley <ryan.quigley32@outlook.com>
2020-04-10 11:23:48 -0400

    update

--- vue_starter_java/frontend/src/components/Family.vue
+++ vue_starter_java/frontend/src/components/Family.vue
@@ -1,140 +1,140 @@
 <template>
   <div>
-    <div>
-      <h2> Family </h2>   
-    </div>
+    <div></div>
     <form class="form-register" @submit.prevent="registerChild">
-    <a v-if="!addChildForm" href="#" 
-    v-on:click="showForm"> Add Child to Family
-    </a>
-    <a v-if="addChildForm" href="#" 
-    v-on:click="hideForm">Hide Form 
-    </a>
-    <span v-if="addChildForm">
-      <label for="username">Username</label>
-    <input
-      type="text"
-      id="username"
-      class="form-control"
-      placeholder="Username"
-      v-model="child.username"
-      required
-      autofocus
-      />
+      <div>
+        Family
+        <a v-if="!addChildForm" href="#" v-on:click="showForm">Add Child</a>
+        <a v-if="addChildForm" href="#" v-on:click="hideForm">Hide Form</a>
+      </div>
+
+      <span v-if="addChildForm">
+        <label for="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          class="form-control"
+          placeholder="Child Username"
+          v-model="child.username"
+          required
+          autofocus
+        />
       </span>
 
       <span v-if="addChildForm">
-      <label for="password">Password</label>
-    <input
-      type="text"
-      id="password"
-      class="form-control"
-      placeholder="Password"
-      v-model="child.password"
-      required
-      autofocus
-      />
+        <label for="password">Password</label>
+        <input
+          type="text"
+          id="password"
+          class="form-control"
+          placeholder="Child Password"
+          v-model="child.password"
+          required
+          autofocus
+        />
       </span>
 
       <span v-if="addChildForm">
-      <label for="firstName">First Name</label>
-    <input
-      type="text"
-      id="firstName"
-      class="form-control"
-      placeholder="First Name"
-      v-model="child.firstName"
-      required
-      autofocus
-      />
+        <label for="firstName">First Name</label>
+        <input
+          type="text"
+          id="firstName"
+          class="form-control"
+          placeholder="First Name"
+          v-model="child.firstName"
+          required
+          autofocus
+        />
       </span>
 
       <span v-if="addChildForm">
-      <label for="lastName">Last Name</label>
-    <input
-      type="text"
-      id="lastName"
-      class="form-control"
-      placeholder="Last Name"
-      v-model="child.lastName"
-      required
-      autofocus
-      />
+        <label for="lastName">Last Name</label>
+        <input
+          type="text"
+          id="lastName"
+          class="form-control"
+          placeholder="Last Name"
+          v-model="child.lastName"
+          required
+          autofocus
+        />
       </span>
 
-      <br>
-      <button v-if="addChildForm" class="create-account-button" type="submit">
-        Submit Child Info
-      </button>
-      <br>
-      </form>
-         <div v-for="user in family" v-bind:key="user">{{user.firstName}} {{user.lastName}}</div>
-
-    </div>    
+      <br />
+      <button v-if="addChildForm" class="create-account-button" type="submit">Submit Child Info</button>
+      <br />
+    </form>
+    <div v-if="!addChildForm">
+      <div v-for="user in family" v-bind:key="user">{{user.firstName}} {{user.lastName}}</div>
+    </div>
+  </div>
 </template>
 
 
 
 <script>
-import axios from 'axios'
+import axios from "axios";
 
 export default {
   props: {
     apiURL: String
-
   },
   data() {
     return {
       child: {
-        username: '',
-        password: '',
-        firstName: '',
-        lastName: ''
+        username: "",
+        password: "",
+        firstName: "",
+        lastName: ""
       },
-      family:{
-      },
+      family: {},
 
-      addChildForm : false
-      }
+      addChildForm: false
+    };
   },
 
   methods: {
-    showForm(){
+    showForm() {
       this.addChildForm = true;
     },
-    hideForm(){
+    hideForm() {
       this.addChildForm = false;
     },
-    registerChild(){
-      axios.post(`${process.env.VUE_APP_REMOTE_API}/api/addChild`, this.child, {
-      headers:{"Authorization" :  'Bearer ' + localStorage.getItem('Authorization')}})
-      .then(response => {
-        console.log(response);
-      })
-      .catch(err => {
-        this.registrationErrors = true;
-        console.log(err);
-      })
+    registerChild() {
+      axios
+        .post(`${process.env.VUE_APP_REMOTE_API}/api/addChild`, this.child, {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("Authorization")
+          }
+        })
+        .then(response => {
+          console.log(response);
+        })
+        .catch(err => {
+          this.registrationErrors = true;
+          console.log(err);
+        });
     }
-
-
   },
-  created(){
-    axios.get(`${process.env.VUE_APP_REMOTE_API}/api/getFamilyList`,{
-        headers:{"Authorization" :  'Bearer ' + localStorage.getItem('Authorization')}})
-    .then(response => {
+  created() {
+    axios
+      .get(`${process.env.VUE_APP_REMOTE_API}/api/getFamilyList`, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("Authorization")
+        }
+      })
+      .then(response => {
         this.family = response.data;
         console.log(this.family);
-    })
-    .catch(error => {
-        console.log(error + ' there was an error')
-    })
-    }
+      })
+      .catch(error => {
+        console.log(error + " there was an error");
+      });
+  }
 };
 </script>
 
 
 
 <style>
-
 </style>
--- vue_starter_java/frontend/src/components/ReadingEvent.vue
+++ vue_starter_java/frontend/src/components/ReadingEvent.vue
@@ -1,43 +1,35 @@
 <template>
-<div>
-  <div class="header">
-    <h2>Reading Event</h2>
+  <div>
+    <div class="header">
+      <h2>Reading Event</h2>
+    </div>
+
+    <select v-on:click="getBooks" id="books">
+      <option v-for="book in books" v-bind:key="book.id" value="book.id">{{book.title}}</option>
+    </select>
+
+    <select v-on:click="selectUser" id="users">
+      <option v-for="user in users" v-bind:key="user.id" value="user.id">{{user.username}}</option>
+    </select>
+
+    <div class="form">
+      <div class="form-input">
+        <span class="label">Enter Reading Time:</span>
+        <input type="text" placeholder="In Minutes" />
+        <div>
+          <button v-on:click="addReadingEvent">Submit</button>
+        </div>
+      </div>
+    </div>
   </div>
-
-  <select v-on:click= "getBooks" id="books">
-   <option v-for="book in books" v-bind:key="book.id" value= book.id>{{book.title}}</option> 
-  </select>
-
-  <select v-on:click= "selectUser" id="users">
-    <option v-for="user in users" v-bind:key="user.id" value= user.id>{{user.username}}</option> 
-  </select>
-
-  <div class="form">
-    <div class="form-input">
-      <span class="label">Enter Reading Time:</span>
-      <input type="text" placeholder="In Minutes">
-    <div>
-      <button v-on:click= "addReadingEvent">Submit</button>
-    </div>
-    </div>
-    </div>
-
-
-
-</div>
-
 </template>
 
 <script>
-
-import axios from 'axios'
-
+import axios from "axios";
 
 export default {
-
   props: {
     apiURL: String
-
   },
   data() {
     return {
@@ -45,59 +37,70 @@ export default {
       users: [],
       readingEvent: 0,
       format: []
-      }
-    },
+    };
+  },
 
   methods: {
     getBooks() {
-       axios.get(`${process.env.VUE_APP_REMOTE_API}/api/getAllBooks`, {
-      headers:{"Authorization" :  'Bearer ' + localStorage.getItem('Authorization')}})
-    .then(response => {
-      console.log(response)
-      this.books = response.data;
-    })
-    .catch(error => {
-        console.log(error + ' there was an error')
-      })
+      axios
+        .get(`${process.env.VUE_APP_REMOTE_API}/api/getAllBooks`, {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("Authorization")
+          }
+        })
+        .then(response => {
+          console.log(response);
+          this.books = response.data;
+        })
+        .catch(error => {
+          console.log(error + " there was an error");
+        });
     },
 
     selectUser() {
-      axios.get(`${process.env.VUE_APP_REMOTE_API}/api/getUser`, {
-      headers:{"Authorization" :  'Bearer ' + localStorage.getItem('Authorization')}})
-    .then(response => {
-      console.log(response)
-      this.users = response.data;
-    })
-    .catch(error => {
-        console.log(error + ' there was an error')
-      })
+      axios
+        .get(`${process.env.VUE_APP_REMOTE_API}/api/getUser`, {
+          headers: {
+            Authorization: "Bearer " + localStorage.getItem("Authorization")
+          }
+        })
+        .then(response => {
+          console.log(response);
+          this.users = response.data;
+        })
+        .catch(error => {
+          console.log(error + " there was an error");
+        });
     },
 
     addReadingEvent() {
-       axios.post(`${process.env.VUE_APP_REMOTE_API}/api/addReadingEvent`, this.readingEvent, {
-      headers:{"Authorization" :  'Bearer ' + localStorage.getItem('Authorization')}})
-    .then(response => {
-      console.log(response)
-    })
-    .catch(error => {
-        console.log(error + ' there was an error')
-      })
+      axios
+        .post(
+          `${process.env.VUE_APP_REMOTE_API}/api/addReadingEvent`,
+          this.readingEvent,
+          {
+            headers: {
+              Authorization: "Bearer " + localStorage.getItem("Authorization")
+            }
+          }
+        )
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => {
+          console.log(error + " there was an error");
+        });
     }
-
-
   },
 
   computed: {
     isValidForm() {
       return this.readingEvent > 0;
-    },
+    }
   }
 };
-    
-    
 </script>
 
 
 <style>
-
 </style>
