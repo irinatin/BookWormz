dd974c11171cfb2a5987343e5804ee69d30b6541
Leigh Hartmann <lahartmann@gmail.com>
2020-04-12 15:34:21 -0400

    bulma updates

--- vue_starter_java/frontend/src/App.vue
+++ vue_starter_java/frontend/src/App.vue
@@ -6,7 +6,7 @@
     <navbar class="navbar">
       <div class="container">
         <div class="navbar-brand">
-        <a class="navbar-item brand-text"><router-link to="/about"><img src="./views/assets/BWLogo.jpg" alt="BookWormz Photo" class="product-img">BookWormz</router-link></a>
+        <a class="navbar-item brand-text"><router-link to="/about"><img src="./views/assets/BWLogo.jpg" alt="BookWormz Photo" class="product-img"/>BookWormz</router-link></a>
           <a role="button" class="navbar-burger" data-target="navbar-menu" onclick="document.querySelector('.navbar-menu').classList.toggle('is-active');" aria-label="menu" aria-expanded="false">
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
@@ -60,10 +60,12 @@ export default {
 .product-img {
   width: auto;
   height: auto;
-
+ 
 }
 .brand-text{
-  padding-left:30%;
+  padding-left: 20%;
+
+ 
 }
 .navbar-start > .navbar-item {
   font-weight: 700;
