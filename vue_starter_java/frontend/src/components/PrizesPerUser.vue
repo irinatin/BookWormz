c9b0f2002e0f7fdf98103c351103cba31d7f4928
Ryan Quigley <ryan.quigley32@outlook.com>
2020-04-15 11:11:07 -0400

    friend and family update

--- vue_starter_java/frontend/src/components/PrizesPerUser.vue
+++ vue_starter_java/frontend/src/components/PrizesPerUser.vue
@@ -1,24 +1,35 @@
 <template>
-    <ul>
-        <li v-for="prize in prizes" v-bind:key="prize">{{prize}}</li>
-    </ul>
-    
+  <ul>
+    <li v-for="prize in prizes" v-bind:key="prize">{{prize}}</li>
+  </ul>
 </template>
 
 <script>
-import axios from 'axios'
-
+import axios from "axios";
+import { eventBus } from "../main.js";
 
 export default {
+  data() {
+    return {
+      prizes: []
+    };
+  },
 
-    data() {
-        return {
-            prizes: []
+  created() {
+    axios
+      .get(`${process.env.VUE_APP_REMOTE_API}/api/getPrizesPerUser`, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("Authorization")
         }
-    },
-
-    created() {
-        axios
+      })
+      .then(response => {
+        this.prizes = response.data;
+      })
+      .catch(error => {
+        console.log(error + " there was an error");
+      });
+    eventBus.$on("refreshReadingEvent", () => {
+      axios
         .get(`${process.env.VUE_APP_REMOTE_API}/api/getPrizesPerUser`, {
           headers: {
             Authorization: "Bearer " + localStorage.getItem("Authorization")
@@ -26,14 +37,11 @@ export default {
         })
         .then(response => {
           this.prizes = response.data;
-          
         })
         .catch(error => {
           console.log(error + " there was an error");
         });
-    }
-
-
-    
-}
+    });
+  }
+};
 </script>
