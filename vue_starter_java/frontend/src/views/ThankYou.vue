27c6660d470fa4408344efa395c4aa16eb0ad1c2
Irina Tin <irina.tin@me.com>
2020-04-15 19:01:26 -0400

    final page

--- vue_starter_java/frontend/src/views/ThankYou.vue
+++ vue_starter_java/frontend/src/views/ThankYou.vue
@@ -2,12 +2,17 @@
  <div class="has-background-white">
     <section class="hero is-primary">
       <div class="container">
-        <div class="columns">
+          <div class="columns">
           <div class="column hero-body">
             <h1 class="title">Thank You, Tech Elevator Pittsburgh!</h1>
           </div>
         </div>
       </div>
+    
+      <img class="photo" src="./assets/Confetti.jpg" style="width:100%;"/>
+
+
+      <!-- <img class="photo" src="./assets/Marissa.png" style="width:25em;"/> -->
       
     </section>
  </div>
@@ -29,6 +34,11 @@ export default {
 </script>
 
 <style scoped>
+
+    .photo {
+        size: 50px;
+    }
+
 
 </style>
 
